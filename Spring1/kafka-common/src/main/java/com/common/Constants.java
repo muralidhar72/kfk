004feee413f9cfd5e5bacd22aede9535c4efd0ca
Pmreddy <vtha.pole@gmail.com>
2020-09-04 15:11:09 +0100

    ddd

---
+++ Spring1/kafka-common/src/main/java/com/common/Constants.java
@@ -0,0 +1,10 @@
+package com.common;
+
+public interface Constants {
+
+    public static String INPUT_TOPIC_NAME = "users";
+    public static String CONSUMER_GROUP = "group_id";
+
+    public static String OUTPUT_EVEN_TOPIC_NAME = "users-even-topic";
+    public static String OUTPUT_ODD_TOPIC_NAME = "users-odd-topic";
+}
---
+++ Spring1/kafka-common/src/main/java/com/common/pojo/Greeting.java
@@ -0,0 +1,43 @@
+package com.common.pojo;
+
+public class Greeting {
+
+
+
+    public String getFrom() {
+        return from;
+    }
+
+
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public Greeting() {
+    }
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String from;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+
+
+}
